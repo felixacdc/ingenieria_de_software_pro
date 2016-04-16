520c39013f43c2e7c163358d901b33a472cf461a
Antonio1394 <antoniorivm1394@gmail.com>
2016-04-15 23:47:33 -0600

    [add] model type user

---
+++ app/app/type_user.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class type_user extends Model
+{
+    //
+    protected $table='type_users';
+
+    protected $fillable=['type_user'];
+}
