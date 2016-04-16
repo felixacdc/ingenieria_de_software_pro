746bb4b47510ebda0400a15ccc7e087e6f50aa69
Antonio1394 <antoniorivm1394@gmail.com>
2016-04-15 23:13:51 -0600

    [add] model type services health

--- app/app/type_services_health.php
+++ app/app/type_services_health.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class type_services_health extends Model
 {
     //
+
+    protected $table='type_services_health';
+
+    protected $fillable=['type_services'];
 }
