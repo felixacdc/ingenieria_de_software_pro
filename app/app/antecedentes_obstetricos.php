fea8f81dcdcb1b78aad51fa6df67cdc52e9060a9
Antonio1394 <antoniorivm1394@gmail.com>
2016-04-16 23:29:28 -0600

    [update] model antecedentes obstetricos

--- app/app/antecedentes_obstetricos.php
+++ app/app/antecedentes_obstetricos.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class antecedentes_obstetricos extends Model
 {
     //
+    protected $table='antecedentes_obstetricos';
+    protected $fillable=[
+      'dato1',
+      'dato2',
+      'dato3',
+      'dato4',
+      'dato5',
+      'dato6',
+      'dato7',
+      'patient_id'
+    ];
 }
