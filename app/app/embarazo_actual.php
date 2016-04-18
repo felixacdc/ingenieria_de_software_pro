95abf91f2b899de4a415550a68e1277be6e1958f
Antonio1394 <antoniorivm1394@gmail.com>
2016-04-17 22:32:46 -0600

    [add] model embarazo actual

--- app/app/embarazo_actual.php
+++ app/app/embarazo_actual.php
@@ -7,4 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class embarazo_actual extends Model
 {
     //
+    protected $table ='embarazo_actual';
+
+    protected $fillable=[
+      'dato1',
+      'dato2',
+      'dato3',
+      'dato4',
+      'dato5',
+      'dato6',
+      'dato7',
+      'dato8',
+      'dato9',
+      'dato10',
+      'dato11',
+      'dato12',
+      'patient_id'
+
+    ];
 }
