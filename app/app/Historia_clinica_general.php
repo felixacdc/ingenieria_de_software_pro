cbb42f5abbfcaaf317f44d231b28b0eee7637d5a
Antonio1394 <antoniorivm1394@gmail.com>
2016-04-17 22:45:57 -0600

    [update] model historial clinico

--- app/app/Historia_clinica_general.php
+++ app/app/Historia_clinica_general.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Historia_clinica_general extends Model
 {
     //
+    protected $table='historia_clinica_general';
+
+    protected $fillable=[
+      'dato1',
+      'dato2',
+      'dato3',
+      'dato4',
+      'dato5',
+      'dato6',
+      'dato7',
+      'patient_id'
+    ];
 }
