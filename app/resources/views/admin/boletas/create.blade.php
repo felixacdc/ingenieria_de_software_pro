4fb3fe9a7093bd221c7859af2b4a46d9ed6e8735
felixacdc <pfmata01@gmail.com>
2016-05-01 14:45:31 -0600

    [modify] container to content

--- app/resources/views/admin/boletas/create.blade.php
+++ app/resources/views/admin/boletas/create.blade.php
@@ -24,29 +24,31 @@
                     <li role="presentation"><a href="#tabData4" data-toggle="tab">Historia Clínica General</a></li>
                     <li role="presentation"><a href="#tabData5" data-toggle="tab">Persona Responsable</a></li>
                 </ul>
+                {!!Form::open(['route' => 'admin.boleta.store', 'method' => 'POST', 'class' => 'form-validate', 'id' => 'createForm'])!!}
+                    <div id="my-tab-content" class="tab-content">
 
-                <div id="my-tab-content" class="tab-content">
-                    <!-- Datos Personales-->
-                    <div class="tab-pane in active animated flipInX" id="tabData1">
-                        @include('admin.boletas.pacientes')
+                        <!-- Datos Personales-->
+                        <div class="tab-pane in active animated flipInX" id="tabData1">
+                            @include('admin.boletas.pacientes')
+                        </div>
+                        <!-- Antecedentes Obstétricos-->
+                        <div class="tab-pane in animated flipInX" id="tabData2">
+                            @include('admin.boletas.antecedentes')
+                        </div>
+                        <!-- Embarazo Actual-->
+                        <div class="tab-pane in animated flipInX" id="tabData3">
+                            @include('admin.boletas.embarazoactual')
+                        </div>
+                        <!-- Historia Clínica General-->
+                        <div class="tab-pane in animated flipInX" id="tabData4">
+                            @include('admin.boletas.historiaclinica')
+                        </div>
+                        <!-- Persona Responsable-->
+                        <div class="tab-pane in animated flipInX" id="tabData5">
+                            @include('admin.boletas.responsable')
+                        </div>
                     </div>
-                    <!-- Antecedentes Obstétricos-->
-                    <div class="tab-pane in animated flipInX" id="tabData2">
-                        @include('admin.boletas.antecedentes')
-                    </div>
-                    <!-- Embarazo Actual-->
-                    <div class="tab-pane in animated flipInX" id="tabData3">
-                        @include('admin.boletas.embarazoactual')
-                    </div>
-                    <!-- Historia Clínica General-->
-                    <div class="tab-pane in animated flipInX" id="tabData4">
-                        @include('admin.boletas.historiaclinica')
-                    </div>
-                    <!-- Persona Responsable-->
-                    <div class="tab-pane in animated flipInX" id="tabData5">
-                        @include('admin.boletas.responsable')
-                    </div>
-                </div>
+                {!!Form::close()!!}
             </div>
         </section>
     </aside>
--- app/resources/views/admin/boletas/pacientes.blade.php
+++ app/resources/views/admin/boletas/pacientes.blade.php
@@ -1,9 +1,16 @@
-<div class="container">
+<div class="content back-white">
     <div class="row">
-        <div class="col-md-2"></div>
-        <div class="col-md-8">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
             <h2>Datos Personales</h2>
-            
+                <div class="form-group col-md-7">
+                	<label for="">Nombre de la embarazada</label>
+                	{!!Form::text('centro', null, array('class' => 'form-control', 'placeholder' => 'Inserta el Nombre del centro', 'required' => 'required'))!!}
+                </div>
+                <div class="form-group col-md-3">
+                	<label for="">Edad en años</label>
+                	{!!Form::text('direccion', null, array('class' => 'form-control', 'placeholder' => 'Inserta la dirección del centro', 'required' => 'required'))!!}
+                </div>
         </div>
     </div>
 </div>
