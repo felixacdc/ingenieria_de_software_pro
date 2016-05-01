0abb2a07e764f00426a3a7b149ff4e6e583d8e10
Antonio1394 <antoniorivm1394@gmail.com>
2016-04-30 23:20:26 -0600

    [update] view

--- app/resources/views/admin/tiposCentros/partials/createForm.blade.php
+++ app/resources/views/admin/tiposCentros/partials/createForm.blade.php
@@ -1,10 +1,1 @@
-{!!Form::open(['route' => 'admin.tiposCentros.store', 'method' => 'POST', 'class' => 'form-validate', 'id' => 'createForm'])!!}
-
-    @include('admin.tiposCentros.partials.inputsForm')
-
-    <div class="modal-footer">
-		{!!Form::submit('Registrar', array('class' => 'btn btn-primary'))!!}
-		<button type="button" class="btn btn-danger" data-dismiss="modal">cerrar</button>
-	</div>
-
-{!!Form::close()!!}
+<h1>sdasd</h1>
