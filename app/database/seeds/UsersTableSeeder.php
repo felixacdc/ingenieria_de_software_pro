90fde79b66921c02b3a95807f2482c6dfecf7faf
Antonio1394 <antoniorivm1394@gmail.com>
2016-05-09 23:25:16 -0600

    [update] fix state user

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -21,6 +21,7 @@ class UsersTableSeeder extends Seeder
         $user->address = 'Suchi';
         $user->email = 'my@muy.com';
         $user->phone = '58564759';
+        $user->state=true;
         $user->centro_id = 2;
         $user->tipo_usuario_id = 1;
         $user->save();
