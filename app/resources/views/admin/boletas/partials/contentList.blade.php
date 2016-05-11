da464d86f8f3aa4d50b465b619304ba6edd8c7db
felixacdc <pfmata01@gmail.com>
2016-05-11 17:17:06 -0600

    [add] fields date begin and final in list ballots

--- app/resources/views/admin/boletas/partials/contentList.blade.php
+++ app/resources/views/admin/boletas/partials/contentList.blade.php
@@ -5,7 +5,25 @@
             <div class="row">
                 <div class="form-group col-md-6">
                 	{!!Form::label('children','Seleccione el centro que desa consultar:')!!}
-                	{!!Form::select('children', $data, null, ['class' => 'form-control', 'required' => 'required'])!!}
+                	{!!Form::select('children', $data, null, ['class' => 'form-control'])!!}
+                </div>
+            </div>
+            <div class="row">
+                <div class="form-group col-md-6">
+                    {!!Form::label('begin_date','Fecha inicial:')!!}
+                	{!!Form::date('begin_date', null, array('class' => 'form-control', 'placeholder' => 'Fecha  última regla', 'required' => 'required'))!!}
+                </div>
+                <div class="form-group col-md-6">
+                    {!!Form::label('final_date','Fecha final:')!!}
+                	{!!Form::date('final_date', null, array('class' => 'form-control', 'placeholder' => 'Fecha  última regla', 'required' => 'required'))!!}
+                </div>
+            </div>
+            <div class="content-report">
+
+            </div>
+            <div class="row">
+                <div class="form-group col-md-12 text-right">
+                    <a href="#" class="btn btn-primary nextButton">Buscar</a>
                 </div>
             </div>
         </div>
