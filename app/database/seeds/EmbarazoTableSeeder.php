a2f44cbc712c102828297aa9d6f0a9a38a1852be
Antonio1394 <antoniorivm1394@gmail.com>
2016-05-16 11:59:14 -0600

    [update] fix seeder

--- app/database/seeds/EmbarazoTableSeeder.php
+++ app/database/seeds/EmbarazoTableSeeder.php
@@ -11,6 +11,22 @@ class EmbarazoTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\embarazo_actual::class,50)->create();
+      for ($i=1; $i <=50 ; $i++) {
+        DB::table('embarazo_actual')->insert([
+          'dato1'          => rand(0,1),
+          'dato2'          => true,
+          'dato3'          => true,
+          'dato4'          => false,
+          'dato5'          => true,
+          'dato6'          => false,
+          'dato7'          => true,
+          'dato8'          => true,
+          'dato9'          => true,
+          'dato10'         => true,
+          'dato11'         => true,
+          'dato12'         => true,
+          'pacientes_id'   => $i,
+      ]);
+      }
     }
 }
