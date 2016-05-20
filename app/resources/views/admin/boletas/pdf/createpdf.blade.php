f11a3324295d443fe0dcef44170805b3175a1ff9
ronygo <ingenieriahacker@outlook.es>
2016-05-20 10:27:59 -0600

    [update] pdf

--- app/resources/views/admin/boletas/pdf/createpdf.blade.php
+++ app/resources/views/admin/boletas/pdf/createpdf.blade.php
@@ -26,28 +26,20 @@
 		</div>	    
 
     </header>
-	
 	<!-- <h1>Listado de registros</h1> -->
-
 	<table>
-	<div style="page-break-after: always;"></div>
+		
+	<!-- <div style="page-break-after: always;"></div> -->
 		<thead>
 			<tr>
-				<th>No. de Registro</th>
-				<th>DAS</th>
-				<th>Distrito</th>
-				<th>Nombre Paciente</th>
-				<th>Edad</th>
-				<th>Pueblo</th>
-				<th>Centro de Salud mas Cercano</th>
-				<th>FUR</th>
-				<th>FPP</th>
-				<th>Embarazos</th>
-				<th>Partos</th>
-				<th>Cesareas</th>
-				<th>Abortos</th>
-				<th>Hijos Vivos</th>
-				<th>Hijos Muertos</th>
+				<th class="C1">No. de Registro</th>
+				<th class="C2">DAS</th>
+				<th class="C3">Nombre Paciente</th>
+				<th class="C4">Edad</th>
+				<th class="C5">Pueblo</th>
+				<th class="C6">Centro de Salud mas Cercano</th>
+				<th class="C7">FUR</th>
+				<th class="C7">FPP</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -55,64 +47,198 @@
 				<tr>
 					<td>{{ $paciente->no_registro }}</td>
 					@foreach($DAS as $centro)
-						<th>{{ $centro->centro }}</th>
+						<td>{{ $centro->centro }}</td>
 					@endforeach
-					<th>{{ $paciente->nombre_paciente }}</th>
-					<th>{{ $paciente->edad_paciente }}</th>
-					<th>{{ $paciente->pueblo_paciente }}</th>
-					<th>{{ $paciente->distancia_se_si_cercana }}</th>
-					<th>{{ $paciente->fecha_ultima_regla }}</th>
-					<th>{{ $paciente->fecha_probable_parto }}</th>
-					<th>{{ $paciente->no_embarazos }}</th>
-					<th>{{ $paciente->no_partos }}</th>
-					<th>{{ $paciente->no_cesarias }}</th>
-					<th>{{ $paciente->no_abortos }}</th>
-					<th>{{ $paciente->no_hijos_vivos }}</th>
-					<th>{{ $paciente->no_hijos_muertos }}</th>					
+					<td>{{ $paciente->nombre_paciente }}</td>
+					<td>{{ $paciente->edad_paciente }}</td>
+					<td>{{ $paciente->pueblo_paciente }}</td>
+					<td>{{ $paciente->distancia_se_si_cercana }}</td>
+					<td>{{ $paciente->fecha_ultima_regla }}</td>
+					<td>{{ $paciente->fecha_probable_parto }}</td>
 				</tr>
 			@endforeach
-		</tbody>	
+		</tbody>
 	</table>
+
 	<table>
-	<div style="page-break-after: always;"></div>
+	<!-- <div style="page-break-after: always;"></div> -->
 		<thead>
 			<tr>
-				<th>No. de Registro</th>
-				<th>DAS</th>
-				<th>Distrito</th>
-				<th>Nombre Paciente</th>
-				<th>Edad</th>
-				<th>Pueblo</th>
-				<th>Centro de Salud mas Cercano</th>
-				<th>FUR</th>
-				<th>FPP</th>
-				<th>Embarazos</th>
-				<th>Partos</th>
-				<th>Cesareas</th>
-				<th>Abortos</th>
-				<th>Hijos Vivos</th>
-				<th>Hijos Muertos</th>
+				<th class="C8">Embarazos</th>
+				<th class="C8">Partos</th>
+				<th class="C8">Cesareas</th>
+				<th class="C8">Abortos</th>
+				<th class="C8">Hijos Vivos</th>
+				<th class="C8">Hijos Muertos</th>
+				<th colspan="7" class="C8">Antecedentes Obstétricos</th>
+				<th colspan="12" class="C3">Embarazo Actual</th>
+				<th colspan="6" class="C8">Historia Clínica General</th>
 			</tr>
 		</thead>
 		<tbody>
 			@foreach($data as $paciente)
 				<tr>
-					<td>{{ $paciente->no_registro }}</td>
-					@foreach($DAS as $centro)
-						<th>{{ $centro->centro }}</th>
-					@endforeach
-					<th>{{ $paciente->nombre_paciente }}</th>
-					<th>{{ $paciente->edad_paciente }}</th>
-					<th>{{ $paciente->pueblo_paciente }}</th>
-					<th>{{ $paciente->distancia_se_si_cercana }}</th>
-					<th>{{ $paciente->fecha_ultima_regla }}</th>
-					<th>{{ $paciente->fecha_probable_parto }}</th>
-					<th>{{ $paciente->no_embarazos }}</th>
-					<th>{{ $paciente->no_partos }}</th>
-					<th>{{ $paciente->no_cesarias }}</th>
-					<th>{{ $paciente->no_abortos }}</th>
-					<th>{{ $paciente->no_hijos_vivos }}</th>
-					<th>{{ $paciente->no_hijos_muertos }}</th>					
+					<td>{{ $paciente->no_embarazos }}</td>
+					<td>{{ $paciente->no_partos }}</td>
+					<td>{{ $paciente->no_cesarias }}</td>
+					<td>{{ $paciente->no_abortos }}</td>
+					<td>{{ $paciente->no_hijos_vivos }}</td>
+					<td>{{ $paciente->no_hijos_muertos }}</td>
+					
+					<!-- Columna 1 -->
+					<td class="celdas">
+						@if($paciente->antecedentes->dato1 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas">
+						@if($paciente->antecedentes->dato2 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas">
+						@if($paciente->antecedentes->dato3 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas">
+						@if($paciente->antecedentes->dato4 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas">
+						@if($paciente->antecedentes->dato5 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas">
+						@if($paciente->antecedentes->dato6 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas">
+						@if($paciente->antecedentes->dato7 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<!-- Columna 2 -->
+					<td class="celdas2">
+						@if($paciente->embarazoActual->dato1 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas2">
+						@if($paciente->embarazoActual->dato2 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas2">
+						@if($paciente->embarazoActual->dato3 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas2">
+						@if($paciente->embarazoActual->dato4 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas2">
+						@if($paciente->embarazoActual->dato5 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas2">
+						@if($paciente->embarazoActual->dato6 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas2">
+						@if($paciente->embarazoActual->dato7 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas2">
+						@if($paciente->embarazoActual->dato8 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas2">
+						@if($paciente->embarazoActual->dato9 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas2">
+						@if($paciente->embarazoActual->dato10 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas2">
+						@if($paciente->embarazoActual->dato11 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas2">
+						@if($paciente->embarazoActual->dato12 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					
+					<!-- Columna 3 -->
+					<td class="celdas">
+						@if($paciente->historiaClinica->dato1 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas">
+						@if($paciente->historiaClinica->dato2 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas">
+						@if($paciente->historiaClinica->dato3 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas">
+						@if($paciente->historiaClinica->dato4 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas">
+						@if($paciente->historiaClinica->dato5 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>
+					<td class="celdas">
+						@if($paciente->historiaClinica->dato6 == 1)
+							<img src="img/si.png" alt="">
+						@else
+							<img src="img/no.png" alt="">
+						@endif</td>	
 				</tr>
 			@endforeach
 		</tbody>	
