da66c4ce9de4bc92eab96543a7bf70809b7463a9
felixacdc <pfmata01@gmail.com>
2016-05-26 15:28:41 -0600

    [modify] validation js for general center edit

--- app/resources/views/admin/users/partials/editForm.blade.php
+++ app/resources/views/admin/users/partials/editForm.blade.php
@@ -11,6 +11,18 @@
 
 
 <script type="text/javascript">
+
+    function valTypeUser(value, element, param) {
+        if ($('#tipo_usuario_id').val() == 1 && value == 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    $.validator.addMethod("valTypeUser", valTypeUser, "El centro general solo puede ser asignado a un Super Administrador.");
+
     $("#createForm, #editForm").validate({
         rules: {
                 user:{
@@ -35,7 +47,8 @@
         					maxlength: 8
         				},
         				centro_id: {
-                            required: true
+                            required: true,
+                            valTypeUser: true
                         },
         				tipo_usuario_id: {
         						required: true
